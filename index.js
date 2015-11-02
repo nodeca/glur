19fc5cdb860b46824f2a4c84614e9e1c30dd50b4
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-11-02 13:46:35 +0300

    Resorted vars/ops to reflect proper data order (rgba)

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var gaussCoef = function (sigma) {
 
 
 var convolveRGBA = function (src, out, tmp, coeff, width, height) {
-  var x, y, rgb, r, g, b, a, out_index, y_offset, x_offset;
+  var x, y, rgba, r, g, b, a, out_index, y_offset, x_offset;
   var r0, g0, b0, a0, r1, g1, b1, a1, r2, g2, b2, a2;
 
   var coeff_b0 = coeff[0];
@@ -37,114 +37,116 @@ var convolveRGBA = function (src, out, tmp, coeff, width, height) {
     out_index = y + height * width - 1;
     y_offset = y * width;
 
-    rgb = src[y_offset];
-    a = (rgb >> 24) & 0xff;
-    r = (rgb >> 16) & 0xff;
-    g = (rgb >> 8) & 0xff;
-    b = rgb & 0xff;
+    rgba = src[y_offset];
 
-    a0 = a;
+    r = rgba & 0xff;
+    g = (rgba >> 8) & 0xff;
+    b = (rgba >> 16) & 0xff;
+    a = (rgba >> 24) & 0xff;
+
     r0 = r;
     g0 = g;
     b0 = b;
+    a0 = a;
 
-    a1 = a0;
     r1 = r0;
     g1 = g0;
     b1 = b0;
+    a1 = a0;
 
-    a2 = a1;
     r2 = r1;
     g2 = g1;
     b2 = b1;
+    a2 = a1;
 
     x_offset = 0;
 
     for (x = 0; x < width; x++) {
-      rgb = src[y_offset + x];
-      a = (rgb >> 24) & 0xff;
-      r = (rgb >> 16) & 0xff;
-      g = (rgb >> 8) & 0xff;
-      b = rgb & 0xff;
+      rgba = src[y_offset + x];
 
-      a = coeff_b0 * a + (coeff_a0 * a0 + coeff_a1 * a1 + coeff_a2 * a2);
+      r = rgba & 0xff;
+      g = (rgba >> 8) & 0xff;
+      b = (rgba >> 16) & 0xff;
+      a = (rgba >> 24) & 0xff;
+
       r = coeff_b0 * r + (coeff_a0 * r0 + coeff_a1 * r1 + coeff_a2 * r2);
       g = coeff_b0 * g + (coeff_a0 * g0 + coeff_a1 * g1 + coeff_a2 * g2);
       b = coeff_b0 * b + (coeff_a0 * b0 + coeff_a1 * b1 + coeff_a2 * b2);
+      a = coeff_b0 * a + (coeff_a0 * a0 + coeff_a1 * a1 + coeff_a2 * a2);
 
-      a2 = a1;
       r2 = r1;
       g2 = g1;
       b2 = b1;
+      a2 = a1;
 
-      a1 = a0;
       r1 = r0;
       g1 = g0;
       b1 = b0;
+      a1 = a0;
 
-      a0 = a;
       r0 = r;
       g0 = g;
       b0 = b;
+      a0 = a;
 
-      tmp[x_offset] = a;
-      tmp[x_offset + 1] = r;
-      tmp[x_offset + 2] = g;
-      tmp[x_offset + 3] = b;
+      tmp[x_offset] = r;
+      tmp[x_offset + 1] = g;
+      tmp[x_offset + 2] = b;
+      tmp[x_offset + 3] = a;
 
       x_offset += 4;
     }
 
-    a0 = a;
     r0 = r;
     g0 = g;
     b0 = b;
+    a0 = a;
 
-    a1 = a0;
     r1 = r0;
     g1 = g0;
     b1 = b0;
+    a1 = a0;
 
-    a2 = a1;
     r2 = r1;
     g2 = g1;
     b2 = b1;
+    a2 = a1;
 
     for (x = width - 1; x >= 0; x--) {
       x_offset -= 4;
 
-      a = tmp[x_offset];
-      r = tmp[x_offset + 1];
-      g = tmp[x_offset + 2];
-      b = tmp[x_offset + 3];
+      r = tmp[x_offset];
+      g = tmp[x_offset + 1];
+      b = tmp[x_offset + 2];
+      a = tmp[x_offset + 3];
 
-      a = coeff_b0 * a + (coeff_a0 * a0 + coeff_a1 * a1 + coeff_a2 * a2);
       r = coeff_b0 * r + (coeff_a0 * r0 + coeff_a1 * r1 + coeff_a2 * r2);
       g = coeff_b0 * g + (coeff_a0 * g0 + coeff_a1 * g1 + coeff_a2 * g2);
       b = coeff_b0 * b + (coeff_a0 * b0 + coeff_a1 * b1 + coeff_a2 * b2);
+      a = coeff_b0 * a + (coeff_a0 * a0 + coeff_a1 * a1 + coeff_a2 * a2);
 
-      a2 = a1;
       r2 = r1;
       g2 = g1;
       b2 = b1;
+      a2 = a1;
 
-      a1 = a0;
       r1 = r0;
       g1 = g0;
       b1 = b0;
+      a1 = a0;
 
-      a0 = a;
       r0 = r;
       g0 = g;
       b0 = b;
+      a0 = a;
 
-      a = (a |0) & 0xff;
       r = (r |0) & 0xff;
       g = (g |0) & 0xff;
       b = (b |0) & 0xff;
+      a = (a |0) & 0xff;
 
       out_index -= height;
-      out[out_index] = (a << 24) | (r << 16) | (g << 8) | b;
+      out[out_index] = (a << 24) | (b << 16) | (g << 8) | r;
     }
   }
   // console.timeEnd('convolve');
