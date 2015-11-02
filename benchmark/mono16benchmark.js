2e445ad568a3f3fa46807a6e55eb65e0615cddf8
Alexander Rodin <rodin.alexander@gmail.com>
2015-11-02 17:17:29 +0300

    Added Mono16 version

---
+++ benchmark/mono16benchmark.js
@@ -0,0 +1,148 @@
+#!/usr/bin/env node
+/*eslint-env node */
+
+'use strict';
+
+
+var path      = require('path');
+var util      = require('util');
+var Benchmark = require('benchmark');
+var ansi      = require('ansi');
+var cursor    = ansi(process.stdout);
+var mono16 = require('../mono16');
+
+var IMPLS = [ {
+  name: 'glur-mono16',
+  code: {
+    run: function (data) {
+      return mono16(data.buffer, data.width, data.height, data.radius);
+    }
+  }
+} ];
+
+var SAMPLES_SRC = [
+  {
+    name: 'Big',
+    width: 3200,
+    height: 2500,
+    radius: 50
+  }
+];
+
+var SAMPLES = [];
+
+
+SAMPLES_SRC.forEach(function (sample) {
+  var content = {};
+
+  content.width  = sample.width;
+  content.height = sample.height;
+  content.radius = sample.radius;
+  content.buffer = new Uint16Array(sample.width * sample.height);
+
+  var title  = util.format('(%d bytes raw / [%dx%d]px)',
+                           content.buffer.length, content.width, content.height);
+
+
+  function onComplete() {
+    cursor.write('\n');
+  }
+
+
+  var suite = new Benchmark.Suite(title, {
+    onError: function (err) {
+      console.log(err.target.error);
+    },
+
+    onStart: function onStart() {
+      console.log('\nSample: %s %s', sample.name, title);
+    },
+
+    onComplete: onComplete
+  });
+
+  IMPLS.forEach(function (impl) {
+    suite.add(impl.name, {
+
+      onCycle: function onCycle(event) {
+        cursor.horizontalAbsolute();
+        cursor.eraseLine();
+        cursor.write(' > ' + event.target);
+      },
+
+      onComplete: onComplete,
+
+      defer: !!impl.code.async,
+
+      fn: function (deferred) {
+        if (impl.code.async) {
+          impl.code.run(content, function() {
+            deferred.resolve();
+            return;
+          });
+        } else {
+          impl.code.run(content);
+          return;
+        }
+      }
+    });
+  });
+
+
+  SAMPLES.push({
+    name: sample.name,
+    title: title,
+    content: content,
+    suite: suite
+  });
+});
+
+
+function select(patterns) {
+  var result = [];
+
+  if (!(patterns instanceof Array)) {
+    patterns = [ patterns ];
+  }
+
+  function checkName(name) {
+    return patterns.length === 0 || patterns.some(function (regexp) {
+      return regexp.test(name);
+    });
+  }
+
+  SAMPLES.forEach(function (sample) {
+    if (checkName(sample.name)) {
+      result.push(sample);
+    }
+  });
+
+  return result;
+}
+
+
+function run(files) {
+  var selected = select(files);
+
+  if (selected.length > 0) {
+    console.log('Selected samples: (%d of %d)', selected.length, SAMPLES.length);
+    selected.forEach(function (sample) {
+      console.log(' > %s', sample.name);
+    });
+  } else {
+    console.log('There isn\'t any sample matches any of these patterns: %s', util.inspect(files));
+  }
+
+  selected.forEach(function (sample) {
+    sample.suite.run();
+  });
+}
+
+module.exports.IMPLS             = IMPLS;
+module.exports.SAMPLES           = SAMPLES;
+module.exports.select            = select;
+module.exports.run               = run;
+
+run(process.argv.slice(2).map(function (source) {
+  return new RegExp(source, 'i');
+}));
---
+++ mono16.js
@@ -0,0 +1,91 @@
+var gaussCoef = function (sigma) {
+  var sigma_inv_4 = sigma * sigma;
+  sigma_inv_4 = 1.0 / (sigma_inv_4 * sigma_inv_4);
+
+  var coef_A = sigma_inv_4 * (sigma * (sigma * (sigma * 1.1442707 + 0.0130625) - 0.7500910) + 0.2546730);
+  var coef_W = sigma_inv_4 * (sigma * (sigma * (sigma * 1.3642870 + 0.0088755) - 0.3255340) + 0.3016210);
+  var coef_B = sigma_inv_4 * (sigma * (sigma * (sigma * 1.2397166 - 0.0001644) - 0.6363580) - 0.0536068);
+
+  var z0_abs = Math.exp(coef_A);
+
+  var z0_real = z0_abs * Math.cos(coef_W);
+  var z0_im = z0_abs * Math.sin(coef_W);
+  var z2 = Math.exp(coef_B);
+
+  var z0_abs_2 = z0_abs * z0_abs;
+
+  var a2 = 1.0 / (z2 * z0_abs_2),
+      a0 = (z0_abs_2 + 2 * z0_real * z2) * a2,
+      a1 = -(2 * z0_real + z2) * a2,
+      b0 = 1.0 - (a0 + a1 + a2);
+
+  return new Float32Array([ b0, a0, a1, a2 ]);
+};
+
+
+var convolveMono16 = function (src, out, tmp, coeff, width, height) {
+  var x, y, out_index, y_offset, x_offset;
+  var v, v0, v1, v2;
+
+  var coeff_b0 = coeff[0];
+  var coeff_a0 = coeff[1];
+  var coeff_a1 = coeff[2];
+  var coeff_a2 = coeff[3];
+
+  for (y = 0; y < height; y++) {
+    out_index = y + height * width - 1;
+    y_offset = y * width;
+
+    v = src[y_offset];
+    v0 = v;
+    v1 = v0;
+    v2 = v1;
+
+    x_offset = 0;
+
+    for (x = 0; x < width; x++) {
+      v = src[y_offset + x];
+      v = coeff_b0 * v + (coeff_a0 * v0 + coeff_a1 * v1 + coeff_a2 * v2);
+
+      v2 = v1;
+      v1 = v0;
+      v0 = v;
+
+      tmp[x_offset] = v;
+      x_offset++;
+    }
+
+    v0 = v;
+    v1 = v0;
+    v2 = v1;
+
+    for (x = width - 1; x >= 0; x--) {
+      x_offset--;
+
+      v = tmp[x_offset];
+      v = coeff_b0 * v + (coeff_a0 * v0 + coeff_a1 * v1 + coeff_a2 * v2);
+
+      v2 = v1;
+      v1 = v0;
+      v0 = v;
+
+      out_index -= height;
+      if (out_index >= 0) { // this magical check is needed to avoid deopts in v8
+        out[out_index] = v;
+      }
+    }
+  }
+};
+
+
+var blurMono16 = function (src, width, height, radius) {
+  var out      = new Uint16Array(width * height),
+      tmp_line = new Float32Array(width);
+
+  var coeff = gaussCoef(radius);
+
+  convolveMono16(src, out, tmp_line, coeff, width, height, radius);
+  convolveMono16(out, src, tmp_line, coeff, height, width, radius);
+};
+
+module.exports = blurMono16;
