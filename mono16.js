e4d492954d5b609ba052370e0f8cc8a53d265bb6
Alexander Rodin <rodin.alexander@gmail.com>
2015-11-05 13:41:32 +0300

    Rewrite realization of IIR Gaussian blur for Mono16 case

--- mono16.js
+++ mono16.js
@@ -1,91 +1,95 @@
-var gaussCoef = function (sigma) {
-  var sigma_inv_4 = sigma * sigma;
-  sigma_inv_4 = 1.0 / (sigma_inv_4 * sigma_inv_4);
+// Calculate Gaussian blur of an image using IIR filter
+// The method is taken from Intel's white paper and code example attached to it:
+// https://software.intel.com/en-us/articles/iir-gaussian-blur-filter
+// -implementation-using-intel-advanced-vector-extensions
 
-  var coef_A = sigma_inv_4 * (sigma * (sigma * (sigma * 1.1442707 + 0.0130625) - 0.7500910) + 0.2546730);
-  var coef_W = sigma_inv_4 * (sigma * (sigma * (sigma * 1.3642870 + 0.0088755) - 0.3255340) + 0.3016210);
-  var coef_B = sigma_inv_4 * (sigma * (sigma * (sigma * 1.2397166 - 0.0001644) - 0.6363580) - 0.0536068);
+function gaussCoef(sigma) {
+  if (sigma < 0.5) {
+    sigma = 0.5;
+  }
 
-  var z0_abs = Math.exp(coef_A);
+  var a = Math.exp(0.726 * 0.726) / sigma;
+  var g1 = Math.exp(-a);
+  var g2 = Math.exp(-2 * a);
+  var k = (1 - g1) * (1 - g1) / (1 + 2 * a * g1 - g2);
+  var a0 = k;
+  var a1 = k * (a - 1) * g1;
+  var a2 = k * (a + 1) * g1;
+  var a3 = -k * g2;
+  var b1 = 2 * g1;
+  var b2 = -g2;
+  var left_corner = (a0 + a1) / (1 - b1 - b2);
+  var right_corner = (a2 + a3) / (1 - b1 - b2);
+  return new Float32Array([ a0, a1, a2, a3, b1, b2, left_corner, right_corner ]);
+}
 
-  var z0_real = z0_abs * Math.cos(coef_W);
-  // var z0_im = z0_abs * Math.sin(coef_W);
-  var z2 = Math.exp(coef_B);
+function convolveMono16(src, out, line, coeff, width, height) {
+  // takes src image and writes the blurred and transposed result into out
 
-  var z0_abs_2 = z0_abs * z0_abs;
+  var prev_src, curr_src, curr_out, prev_out, prev_prev_out;
+  var src_index, out_index, line_index;
+  var i, j;
+  var t0, t1, t2, t3;
 
-  var a2 = 1.0 / (z2 * z0_abs_2),
-      a0 = (z0_abs_2 + 2 * z0_real * z2) * a2,
-      a1 = -(2 * z0_real + z2) * a2,
-      b0 = 1.0 - (a0 + a1 + a2);
+  for (i = 0; i < height; i++) {
+    src_index = i * width;
+    out_index = i;
+    line_index = 0;
 
-  return new Float32Array([ b0, a0, a1, a2 ]);
-};
+    // left to right
+    prev_src = src[src_index];
+    prev_prev_out = prev_src * coeff[6];
+    prev_out = prev_prev_out;
 
+    for (j = 0; j < width; j++) {
+      curr_src = src[src_index];
+      t0 = curr_src * coeff[0];
+      t1 = prev_src * coeff[1];
+      t2 = prev_out * coeff[4];
+      t3 = prev_prev_out * coeff[5];
+      curr_out = t0 + t1 + t2 + t3;
 
-function clampTo16(i) { return i < 0 ? 0 : (i > 65535 ? 65535 : i); }
+      prev_prev_out = prev_out;
+      prev_out = curr_out;
+      prev_src = curr_src;
 
-
-var convolveMono16 = function (src, out, tmp, coeff, width, height) {
-  var x, y, out_offs, in_offs, line_buf_offs;
-  var v, v0, v1, v2;
-
-  var coeff_b0 = coeff[0];
-  var coeff_a0 = coeff[1];
-  var coeff_a1 = coeff[2];
-  var coeff_a2 = coeff[3];
-
-  for (y = 0; y < height; y++) {
-    in_offs = y * width;
-
-    v = src[in_offs];
-    v0 = v;
-    v1 = v0;
-    v2 = v1;
-
-    line_buf_offs = 0;
-
-    for (x = 0; x < width; x++) {
-      v = src[in_offs];
-
-      in_offs++;
-
-      v = coeff_b0 * v + (coeff_a0 * v0 + coeff_a1 * v1 + coeff_a2 * v2);
-
-      v2 = v1;
-      v1 = v0;
-      v0 = v;
-
-      tmp[line_buf_offs] = v;
-
-      line_buf_offs++;
+      line[line_index] = prev_out;
+      line_index++;
+      src_index++;
     }
+    src_index--;
+    line_index--;
+    out_index += height * (width - 1);
 
-    v0 = v;
-    v1 = v0;
-    v2 = v1;
+    // right to left
+    prev_src = src[src_index];
+    prev_prev_out = prev_src * coeff[7];
+    prev_out = prev_prev_out;
+    curr_src = prev_src;
 
-    out_offs = y + height * width;
+    for (j = width - 1; j >= 0; j--) {
+      t0 = curr_src * coeff[2];
+      t1 = prev_src * coeff[3];
+      t2 = prev_out * coeff[4];
+      t3 = prev_prev_out * coeff[5];
+      curr_out = t0 + t1 + t2 + t3;
+      prev_prev_out = prev_out;
+      prev_out = curr_out;
 
-    for (x = width - 1; x >= 0; x--) {
-      line_buf_offs--;
+      prev_src = curr_src;
+      curr_src = src[src_index];
 
-      v = tmp[line_buf_offs];
-      v = coeff_b0 * v + (coeff_a0 * v0 + coeff_a1 * v1 + coeff_a2 * v2);
+      out[out_index] = line[line_index] + prev_out;
 
-      v2 = v1;
-      v1 = v0;
-      v0 = v;
-
-      out_offs -= height;
-
-      out[out_offs] = clampTo16((v + 0.5) |0);
+      src_index--;
+      line_index--;
+      out_index -= height;
     }
   }
-};
+}
 
 
-var blurMono16 = function (src, width, height, radius) {
+function blurMono16(src, width, height, radius) {
   // Quick exit on zero radius
   if (!radius) { return; }
 
@@ -96,6 +100,6 @@ var blurMono16 = function (src, width, height, radius) {
 
   convolveMono16(src, out, tmp_line, coeff, width, height, radius);
   convolveMono16(out, src, tmp_line, coeff, height, width, radius);
-};
+}
 
 module.exports = blurMono16;
